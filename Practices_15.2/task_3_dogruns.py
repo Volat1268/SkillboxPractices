397c0f2f2f1b8ceb3547010fa529c93de72e8ca2
aleksandr_markhel <alemarkhel@yandex.ru>
2022-06-13 22:17:48 +0300

    task 3_dogruns.py solved

--- Practices_15.2/task_3_dogruns.py
+++ Practices_15.2/task_3_dogruns.py
@@ -1,19 +1,28 @@
 dogs_count = int(input('Количество собак, участвующих в бегах: '))
+print()
 dogs_points = []
 for dog in range(dogs_count):
     print('Введите количество очков для', dog + 1, 'собаки: ', end='')
     points = int(input())
     dogs_points.append(points)
-print('Очки собак:', dogs_points)
+print('\nТабло с баллами у собак:', dogs_points)
 
-maks = minim = dogs_points[0]
+maks = dogs_points[0]
+minim = dogs_points[0]
+i_count = 0
+for i in dogs_points:
+    if i <= minim:
+        minim = i
+        i_count_minim = i_count
+    if i >= maks:
+        maks = i
+        i_count_maks = i_count
+    i_count += 1
+
+dogs_points[i_count_minim], dogs_points[i_count_maks] = dogs_points[i_count_maks], dogs_points[i_count_minim]
+new_list = []
 
 for i in dogs_points:
-    if i < minim:
-        minim = i
-    if i > maks:
-        maks = i
-print('maks:', maks, 'min', minim)
-print(dogs_points.index(maks))
-print(dogs_points.index(minim))
+    new_list.append(i)
+print('Исправленное табло с баллами у собак: ', new_list)
 
