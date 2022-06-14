501cef140a8319cd21ee365d0699b21866032904
Aleks_work <alemarkhel@yandex.ru>
2022-06-14 08:44:55 +0300

    task_3_dogruns.py: еще раз поправил решение, ввел i_count_minim, i_count_maks = 0

--- Practices_15.2/task_3_dogruns.py
+++ Practices_15.2/task_3_dogruns.py
@@ -10,18 +10,20 @@ print('\nТабло с баллами у собак:', dogs_points)
 maks = dogs_points[0]
 minim = dogs_points[0]
 i_count = 0
+i_count_minim = 0
+i_count_maks = 0
 for i in dogs_points:
-    if i <= minim:
+    if i < minim:
         minim = i
         i_count_minim = i_count
-    if i >= maks:
+    if i > maks:
         maks = i
         i_count_maks = i_count
     i_count += 1
 
 dogs_points[i_count_minim], dogs_points[i_count_maks] = dogs_points[i_count_maks], dogs_points[i_count_minim]
 new_list = []
-
+i_count = 0
 for i in dogs_points:
     new_list.append(i)
 print('Исправленное табло с баллами у собак: ', new_list)
