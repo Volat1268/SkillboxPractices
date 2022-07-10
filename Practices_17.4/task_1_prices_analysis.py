ddd4ba2471603ff46dd7b2b31445c4cd969586fb
Aliaksandr Markhel <volatpy@gmail.com>
2022-07-10 21:45:46 +0300

    solve task 1 of practices 17.4
    task 1 solved

--- Practices_17.4/task_1_prices_analysis.py
+++ Practices_17.4/task_1_prices_analysis.py
@@ -1,1 +1,10 @@
+import random
 
+original_prices = [random.randint(-25, 25) for price in range(5)]
+
+new_prices = [0 if price < 0 else price for price in original_prices]
+
+print('Список с предыдущими ценами:', original_prices)
+print('Список с новыми ценами:', new_prices)
+
+print("\nМы потеряли:", abs(sum(original_prices) - sum(new_prices)))
