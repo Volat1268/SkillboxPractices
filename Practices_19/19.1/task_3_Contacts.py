42e17f8bcd8f67de32340ad8a8e93e5b5d6e47c0
aleksandr_markhel <volatpy@gmail.com>
2022-08-09 21:47:28 +0300

    solve task3 / 19.1 / Practices_19 add dir.19.2 / Practices_19

--- Practices_19/19.1/task_3_Contacts.py
+++ Practices_19/19.1/task_3_Contacts.py
@@ -1,25 +1,16 @@
-notebook = dict()
-names_lst = []
-tel_lst = []
 print('Текущие контакты на телефоне:\n')
+notebook = dict()
 while True:
     name = input('Введите имя: ')
     if name == 'Стоп':
-        print('Запись контактов прекращается.')
+        print('Запись контактов прекращена.')
         break
     else:
-        while name in names_lst:
+        while name in notebook:
             print('Ошибка: такое имя уже существует.')
             name = input('Введите имя: ')
-        notebook['Имя'] = names_lst
-        names_lst.append(name)
         tel = input('Введите номер телефона: ')
-        notebook['Телефон'] = tel_lst
-        tel_lst.append(tel)
-        print('\nТекущие контакты на телефоне:')
-        for i_person in range(len(names_lst)):
-            print('{0}   {1}'.format(notebook['Имя'][i_person], notebook['Телефон'][i_person]))
-        print()
-
-
-
+        notebook[name] = tel
+        print(notebook)
+        for contact in notebook:
+            print('{0}   {1}'.format(contact, notebook[contact]))
