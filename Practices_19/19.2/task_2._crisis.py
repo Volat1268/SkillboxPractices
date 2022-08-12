e25b0883ca2031a7448c6b690a732b8e3e44c85d
Aleks_work <alemarkhel@yandex.ru>
2022-08-12 18:39:53 +0300

    solve tasks 1 and 2 / 19.2 / Practice_19

---
+++ Practices_19/19.2/task_2._crisis.py
@@ -0,0 +1,33 @@
+incomes = {
+
+    'apple': 5600.20,
+
+    'orange': 3500.45,
+
+    'banana': 5000.00,
+
+    'bergamot': 3700.56,
+
+    'durian': 5987.23,
+
+    'grapefruit': 300.40,
+
+    'peach': 10000.50,
+
+    'pear': 1020.00,
+
+    'persimmon': 310.00,
+
+}
+
+all_income = 0
+for v_item in sorted(incomes.values()):
+    all_income += v_item
+print('Общий доход за год составил {} рублей'.format(all_income))
+print('Самый маленький доход у {title}. Он составляет {summ} руб.'
+      .format(title=min(incomes, key=incomes.get), summ=incomes[min(incomes, key=incomes.get)]))
+incomes.pop(min(incomes, key=incomes.get))
+print('Итоговый словарь:', incomes)
+
+
+
